2929400649ae4079d174a829d5252d471b3d802f
Ou-linx <oulingxing@outlook.com>
2023-07-11 19:45:04 +0800

    数据库操作移动到工具包文件

--- Utils.py
+++ Utils.py
@@ -1,3 +1,5 @@
+import json
+import mysql.connector
 import yaml
 import requests
 
@@ -31,6 +33,57 @@ class Tools:
         return cookies
 
 
+class DatabaseConnector:
+    cnx = None
+    cursor = None
+    config = None
+
+
+    @staticmethod
+    def connect_to_database():
+        try:
+            DatabaseConnector.cnx = mysql.connector.connect(**DatabaseConnector.config)
+            DatabaseConnector.cursor = DatabaseConnector.cnx.cursor()
+            # print("数据库连接成功！")
+        except mysql.connector.Error as err:
+            print(f"数据库连接错误: {err}")
+            raise
+
+    @staticmethod
+    def initialize_database():
+        if not DatabaseConnector.cnx or not DatabaseConnector.cursor:
+            DatabaseConnector.config = Tools.get_config('database')
+            DatabaseConnector.connect_to_database()
+
+    @staticmethod
+    def run_sql(sqlcmd, params=None):
+        DatabaseConnector.initialize_database()
+        DatabaseConnector.cursor.execute(sqlcmd, params)
+        if DatabaseConnector.cursor.with_rows:
+            rows = DatabaseConnector.cursor.fetchall()
+            column_names = DatabaseConnector.cursor.column_names
+            result = []
+            for row in rows:
+                result.append(dict(zip(column_names, row)))
+            return result
+        else:
+            return DatabaseConnector.cursor.rowcount
+
+    @staticmethod
+    def print_results(sqlcmd, params=None):
+        result = DatabaseConnector.run_sql(sqlcmd, params)
+        if isinstance(result, int):
+            print(f"受影响的行数：{result}")
+        else:
+            json_result = json.dumps(result, indent=4, ensure_ascii=False)
+            print(json_result)
+
+    @staticmethod
+    def close_connection():
+        if DatabaseConnector.cursor:
+            DatabaseConnector.cursor.close()
+        if DatabaseConnector.cnx:
+            DatabaseConnector.cnx.close()
 
 # a = Tools.cookie_set()
 # print(a)
