0c60e784cbce3f729d7c1cfd429af74e478b9676
Ou-linx <oulingxing@outlook.com>
2023-12-09 03:46:46 +0800

    update dh_list/views.py
    Signed-off-by: Ou-linx <oulingxing@outlook.com>

--- dh_list/views.py
+++ dh_list/views.py
@@ -42,10 +42,28 @@ def get_account1(request):  # 查询（单个）
     try:
         acc_id = request.GET.get('id', None)
         if acc_id is not None:
-            sel_data = GameAccount.objects.filter(id=acc_id).select_related('guard_id').first()
-            print(sel_data.guard_id)
-            print(sel_data)
-            return JsonResponse(sel_data, safe=False)
+            sel_data = GameAccount.objects.get(id=acc_id)
+            # print(sel_data.guard_id)
+            # print(sel_data.guard_id.id)
+            res_json = {
+                "guard_id": sel_data.guard_id.id,  # 舰长表id
+                "guard_bili_uid": sel_data.guard_id.bili_uid,  # 舰长b站uid
+                "guard_bili_name": sel_data.guard_id.Bili_name,  # B站用户名
+                "guard_nick_name": sel_data.guard_id.nick_name,  # 舰长自定义名称
+                "guard_rank": sel_data.guard_id.guard_rank,  # 排行榜
+                "guard_level": sel_data.guard_id.guard_level,  # 舰长等级
+                "guard_medal_level": sel_data.guard_id.guard_medal,  # 粉丝牌等级
+                "acc_id": sel_data.id,  # 账号id
+                "username": sel_data.game_acc,  # 用户名
+                "password": sel_data.game_pass,  # 密码
+                "info": sel_data.info,  # 备注
+                "game_class": sel_data.game_class,    # 账号分类
+                "good_friend": sel_data.free, # 不打号标注
+                "update_time": sel_data.update_time,    # 更新时间
+                "is_ok": sel_data.is_ok,     # 打号完成标志
+            }
+            return JsonResponse(res_json, safe=False)
         return HttpResponse("no data get：id")
-    except Exception:
+    except Exception as e:
+        print(e)
         return HttpResponse('error')
